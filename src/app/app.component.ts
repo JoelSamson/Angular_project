344a76d2c869faf9ff2d60a49eebfa6ff28d9f3a
JoelSamson <jsamson6@gmu.edu>
2024-02-13 13:50:06 -0500

    color-changer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,35 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hw';
+  sections = [
+    {
+      title: 'Directives',
+      body: 'The Angular directive helps us to manipulate the DOM...',
+      color: 'red',
+    },
+    {
+      title: 'Pipes',
+      body: 'The Angular pipes are used to Transform the Data...',
+      color: 'green',
+    },
+    {
+      title: 'Component Life Cycle Hook',
+      body: 'The life cycle hooks are the methods that angular invokes...',
+      color: 'blue',
+    },
+    {
+      title: 'HTTP',
+      body: 'The newly designed HttpClient Module allows us to query...',
+      color: 'orange',
+    },
+  ];
+
+  titleColor: string = 'black';
+
+  onSectionClicked(color: string) {
+    this.titleColor = color;
+  }
 }
